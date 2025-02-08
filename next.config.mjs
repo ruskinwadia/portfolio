faa32ea66ef0e0a2c64b6f100831dd9658f498d2
ruskinwadia <ruskinwadia@gmail.com>
2025-02-08 18:27:42 +0530

    lesgo deployy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export", // enable static export
+    basePath: "/portfolio",      // substitute with your repo name
+    assetPrefix: "/portfolio/",  // substitute with your repo name, note the trailing slash
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
